4be73beca72d83958ec94152c6d8474ebe0b5082
RhanieryCorreia <haniery.rh@gmail.com>
2025-12-11 12:12:04 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,31 +4,33 @@ const port = 8000;
 const express = require("express");
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/Sorteiolegal", (request, response) => {
-    response.render("Sorteio legal");
+app.get("/", (request, response) => {
+    response.render("Sorteio legal", {msg: "aguardando número"});
 });
 
-let x
+let x = 0
 
-app.get("/sorteio", (req, res) => {
+app.post("/sorteio", (req, res) => {
 
-    x = (Math.floor(Math.random() * 21));
-    res.send('Novo número sorteado')
+    x = Number((Math.floor(Math.random() * 21)));
+    console.log(x)
+    res.render("Sorteio legal", {msg: "aguardando número"})
 })
 
-
-app.get("/", (req, res) =>{
-    nome = req.query.usuario
-    let msg = "Aguardando número ser enviado"
-    if( usuario == x  ){
+app.post("/sorteiolegal", (req, res) =>{
+    nome = Number(req.body.usuario)
+    
+    msg = ""
+    if( nome == x  ){
     msg = "Parábens você acertou."       
-    } else if( usuario < x){
+    } else if( nome < x){
     msg = " Seu número está abaixo do sorteado."
-    } else if(usuario > x){
+    } else{
      msg = "Seu número está acima do sorteado."
-    }    
-        res.render("Sorteio legal",{usuario, x, msg})
+    }
+    res.render("Sorteio legal", {msg})
 })
 
 app.listen(port, () => {
